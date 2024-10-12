bc884429b38227eb220dbaf1a3aaafbbce261464
Brayden Idzenga <braydenidzenga87@gmail.com>
2024-10-12 02:43:43 -0400

    created user model

---
+++ api/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new  mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unqiue: true
+    },
+    posts: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        required: true,
+        default: []
+    },
+    banned: {
+        type: boolean,
+        required: true,
+        default: false
+    },
+    createdAt: {
+        type: String,
+        required: true,
+        default: Date.now()
+    }
+});
+
+export default mongoose.model("User", userSchema);
