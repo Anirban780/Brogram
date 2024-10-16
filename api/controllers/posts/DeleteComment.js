090c92bf50e3cddc67d127992373a2a6ca58add6
Brayden Idzenga <braydenidzenga87@gmail.com>
2024-10-16 01:30:09 -0400

    created delete comment route

---
+++ api/controllers/posts/DeleteComment.js
@@ -0,0 +1,60 @@
+import { Post, CommentSchema } from "../../models/Post.js";
+import { verifyToken } from "../../util/token.js";
+
+async function DeleteComment(req) {
+    let resStatus = 200;
+    let resMessage = {};
+    
+    // verify that the user is authenticated
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    const user = await verifyToken(token);
+    if (user === null) {
+        resStatus = 400;
+        resMessage = {"Error": "Not authenticated"};
+
+        return { resStatus, resMessage };
+    }
+
+    const { postId, commentId } = req.body;
+
+    try {
+        // find post in database
+        const post = await Post.findOne({_id: postId});
+        if (post === null) {
+            resStatus = 400;
+            resMessage = {"Error": "Post not found."};
+            return {resStatus, resMessage};
+        }
+
+        // find the comment and check if the user posted the comment
+        const comment = post.comments.id(commentId);
+        if (comment === null) {
+            resStatus = 400;
+            resMessage = {"Error": "Comment not found"};
+            return {resStatus, resMessage};
+        }
+
+        if (!comment.creator.equals(user._id)) {
+            resStatus = 400;
+            resMessage = {"Error": "Not authorized"};
+            return {resStatus, resMessage};
+        }
+
+        // delete the comment and save changes
+        comment.deleteOne();
+        post.commentCount--;
+        await post.save();
+
+        resStatus = 200;
+        resMessage = {"Message": "Comment deleted"};
+        return {resStatus, resMessage};
+    } catch (err) {
+        console.log(err);
+        resStatus = 500;
+        resMessage = {"Message": "Internal server error"};
+    }
+}
+
+export default DeleteComment;
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -4,6 +4,7 @@ import newPost from "../controllers/posts/NewPost.js";
 import likePost from "../controllers/posts/LikePost.js";
 import deletePost from "../controllers/posts/DeletePost.js";
 import comment from "../controllers/posts/Comment.js";
+import deleteComment from "../controllers/posts/DeleteComment.js";
 
 const postRouter = express.Router();
 
@@ -20,10 +21,15 @@ postRouter.post("/posts/likepost", async (req,res) => {
 postRouter.post("/posts/deletepost", async (req,res) => {
     const {resStatus, resMessage} = await deletePost(req);
     res.status(resStatus).json(resMessage);
-})
+});
 
 postRouter.post("/posts/comment", async (req, res) => {
     const {resStatus, resMessage} = await comment(req);
+    res.status(resStatus).json(resMessage);
+});
+
+postRouter.post("/posts/deletecomment", async (req, res) => {
+    const {resStatus, resMessage} = await deleteComment(req);
     res.status(resStatus).json(resMessage);
 })
 
